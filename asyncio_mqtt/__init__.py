4403fbc001e4c6ca7da32f6cdf023ea7b6cd9ae9
tropxy <andre14x@gmail.com>
2021-06-18 15:32:40 +0100

    Added ProtocolVersion to the __init__.py to make it possible to address it directly calling the lib module

--- asyncio_mqtt/__init__.py
+++ asyncio_mqtt/__init__.py
@@ -1,10 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from .error import MqttError, MqttCodeError
-from .client import Client, Will
+from .client import Client, Will, ProtocolVersion
 from .version import __version__
 
 __all__ = ["MqttError",
            "MqttCodeError",
            "Client",
            "Will",
+           "ProtocolVersion",
            "__version__"]
