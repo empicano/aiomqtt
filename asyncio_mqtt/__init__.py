c1cbad86287e3fe79155588750a90f3ce915330a
Felix Böhm <felix@felixboehm.dev>
2022-10-05 18:40:31 +0200

    Fix 'asyncio_mqtt' has no attribute 'TLSParameters' error

--- asyncio_mqtt/__init__.py
+++ asyncio_mqtt/__init__.py
@@ -1,11 +1,14 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from .error import MqttError, MqttCodeError
-from .client import Client, Will, ProtocolVersion
+from .client import Client, Will, ProtocolVersion, TLSParameters
 from .version import __version__
 
-__all__ = ["MqttError",
-           "MqttCodeError",
-           "Client",
-           "Will",
-           "ProtocolVersion",
-           "__version__"]
+__all__ = [
+    "MqttError",
+    "MqttCodeError",
+    "Client",
+    "Will",
+    "ProtocolVersion",
+    "TLSParameters",
+    "__version__",
+]
