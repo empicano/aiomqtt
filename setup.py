dc98fbb0f94d4fa495056c9bc5454af034e2c6ca
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2022-10-30 17:29:22 +0100

    Drop Python 3.6 support

--- setup.py
+++ setup.py
@@ -30,8 +30,12 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     keywords="mqtt async asyncio paho-mqtt wrapper",
     install_requires=[
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 
 @pytest.fixture
-def anyio_backend() -> Tuple[str, Dict[str, Any]]:  # pragma: no cover
+def anyio_backend() -> Tuple[str, Dict[str, Any]]:
     if sys.platform == "win32":
         from asyncio.windows_events import WindowsSelectorEventLoopPolicy
 
