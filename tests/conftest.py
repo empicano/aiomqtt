863617eebb41a6fa21890022ef7befc7abf6b651
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2022-10-30 16:38:13 +0100

    Fix Windows tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
+import sys
+from typing import Any, Dict, Tuple
+
 import pytest
 
 
 @pytest.fixture
-def anyio_backend() -> str:
-    return "asyncio"
+def anyio_backend() -> Tuple[str, Dict[str, Any]]:
+    if sys.platform == "win32":
+        from asyncio.windows_events import WindowsSelectorEventLoopPolicy
+
+        return ("asyncio", {"policy": WindowsSelectorEventLoopPolicy()})
+    else:
+        return ("asyncio", {})
