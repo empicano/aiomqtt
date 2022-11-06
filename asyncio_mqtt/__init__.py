f430952e13b0a71639c6b247e87e59f22a12abbb
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2022-11-06 13:56:33 +0100

    Fix __version__ import error

--- asyncio_mqtt/__init__.py
+++ asyncio_mqtt/__init__.py
@@ -1,15 +1,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
+from ._version import __version__, __version_tuple__
 from .client import Client, ProtocolVersion, ProxySettings, TLSParameters, Will
 from .error import MqttCodeError, MqttError
-from .version import __version__
 
 __all__ = [
-    "MqttError",
-    "MqttCodeError",
-    "Client",
-    "Will",
-    "ProxySettings",
-    "ProtocolVersion",
-    "TLSParameters",
     "__version__",
+    "__version_tuple__",
+    "Client",
+    "ProtocolVersion",
+    "ProxySettings",
+    "TLSParameters",
+    "Will",
+    "MqttCodeError",
+    "MqttError",
 ]
--- asyncio_mqtt/version.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("asyncio-mqtt")
-except PackageNotFoundError:
-    pass
