ed460f40c2354938334be5787e9f4342e27e9537
vvanglro <947001731@qq.com>
2023-05-15 15:32:50 +0800

    fix: use Self type hint

--- asyncio_mqtt/client.py
+++ asyncio_mqtt/client.py
@@ -29,12 +29,13 @@ from typing import (
     cast,
 )
 
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
 if sys.version_info >= (3, 10):
     from typing import Concatenate, ParamSpec, TypeAlias
-    if sys.version_info >= (3, 11):
-        from typing import Self
-    else:
-        from typing_extensions import Self
 else:
     from typing_extensions import Concatenate, ParamSpec, TypeAlias
 
