aedaf149c404dd3da3af9162ed7f0e2e427c6d4c
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2023-09-19 11:45:38 +0200

    fix: Release lock in `__aexit__` only if acquired (#249)

--- tests/test_client.py
+++ tests/test_client.py
@@ -87,6 +87,15 @@ async def test_topic_matches() -> None:
     assert not topic.matches("$test/group/a/b/c")
 
 
+async def test__aexit__() -> None:
+    """Test that it's possible to call __aexit__ without prior __aenter__.
+
+    This should also cover the case of an unsucessful __aenter__.
+    """
+    client = Client(HOSTNAME)
+    await client.__aexit__(None, None, None)
+
+
 @pytest.mark.network
 async def test_multiple_messages_generators() -> None:
     """Test that multiple Client.messages() generators can be used at the same time."""
