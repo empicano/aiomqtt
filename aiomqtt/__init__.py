b17c310a0ee212279cbb8bfddc1d18068fe616b1
vvanglro <vvanglro@gmail.com>
2024-02-02 16:24:32 +0800

    fix: aiomqtt has no attribute __version__

--- aiomqtt/__init__.py
+++ aiomqtt/__init__.py
@@ -10,6 +10,10 @@ from .exceptions import MqttCodeError, MqttError, MqttReentrantError
 from .message import Message
 from .topic import Topic, TopicLike, Wildcard, WildcardLike
 
+# These are placeholders that are managed by poetry-dynamic-versioning
+__version__ = "0.0.0"
+__version_tuple__ = (0, 0, 0)
+
 __all__ = [
     "__version__",
     "__version_tuple__",
