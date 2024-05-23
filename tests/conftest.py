a7f6dadd1db0e0a29a02691ac24c6ec17887f074
Matthias Urlichs <matthias@urlichs.de>
2024-05-23 14:01:28 +0200

    Test with asyncio *and* trio

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,6 @@ import pytest
 
 
 # No longer needed, this also works with Trio
-@pytest.fixture
-def anyio_backend() -> tuple[str, dict[str, Any]]:
-    return ("trio", {})
+#@pytest.fixture
+#def anyio_backend() -> tuple[str, dict[str, Any]]:
+#    return ("trio", {})
