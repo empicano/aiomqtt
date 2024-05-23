5a5e64bcd4cc7d857f6dd443d9c17542f6a8cc3d
Matthias Urlichs <matthias@urlichs.de>
2024-05-23 15:14:39 +0200

    Topic: refactor
    Drop the trailing hash from the split wildcard topic.
    
    This further simplifies the code and prepares for the next bit.

--- aiomqtt/topic.py
+++ aiomqtt/topic.py
@@ -13,14 +13,96 @@ else:
 MAX_TOPIC_LENGTH = 65535
 
 
-def _split(self):
+def _split_topic(self):
     if not isinstance(self.value, str):
         return ()
-    return self.value.split("/")
+    return tuple(self.value.split("/"))
 
 
 @attrs.frozen
-class Wildcard:
+class Topic:
+    """MQTT topic that can be published and subscribed to.
+
+    Args:
+        value: The topic string.
+
+    Attributes:
+        value: The topic string.
+    """
+
+    value: str = attrs.field()
+    levels: List[str] = attrs.field(
+        repr=False, hash=False, default=attrs.Factory(_split_topic, takes_self=True)
+    )
+
+    @value.validator
+    def check(self, attribute, value) -> None:
+        """Validate the topic."""
+        if not isinstance(value, str):
+            msg = "Wildcard must be of type str"
+            raise TypeError(msg)
+        if (
+            len(value) == 0
+            or len(value) > MAX_TOPIC_LENGTH
+            or "+" in value or "#" in value
+        ):
+            msg = f"Invalid topic: {self.value}"
+            raise ValueError(msg)
+
+    def matches(self, wildcard: WildcardLike) -> bool:
+        """Check if the topic matches a given wildcard.
+
+        Args:
+            wildcard: The wildcard to match against.
+
+        Returns:
+            True if the topic matches the wildcard, False otherwise.
+        """
+        if not isinstance(wildcard, Wildcard):
+            wildcard = Wildcard(wildcard)
+
+        topic_levels = self.levels
+        wildcard_levels = wildcard.levels
+        if len(wildcard_levels) > 0 and wildcard_levels[0] == "$share":
+            # Shared subscriptions use the topic structure: $share/<group_id>/<topic>
+            wildcard_levels = wildcard_levels[2:]
+
+        # If the topic is shorter than the wildcard it can't match
+        if len(topic_levels) < len(wildcard_levels):
+            return False
+
+        # Unless the wildcard has a trailing '#', if it is longer than the
+        # wildcard it can't match either
+        if not wildcard.prefix and len(wildcard_levels) < len(topic_levels):
+            return False
+
+        # now compare the individual pieces
+        for t, w in zip(topic_levels, wildcard_levels):
+            if t != w and w != "+":
+                return False
+        return True
+
+
+TopicLike: TypeAlias = "str | Topic"
+
+
+
+def _split_wildcard(self):
+    if not isinstance(self.value, str):
+        return ()
+    res = self.value.split("/")
+    if res[-1] == '#':
+        res.pop()
+    return tuple(res)
+
+def _has_hash(self):
+    if not isinstance(self.value, str):
+        return False
+    return self.value == '#' or self.value.endswith('/#')
+
+
+@attrs.frozen
+class Wildcard(Topic):
     """MQTT wildcard that can be subscribed to, but not published to.
 
     A wildcard is similar to a topic, but can optionally contain ``+`` and ``#``
@@ -32,13 +114,15 @@ class Wildcard:
 
     Attributes:
         value: The wildcard string.
-        levels: The topic string, pre-split.
+        levels: The wildcard string, pre-split, without trailing '#' if any.
+        prefix: indicates that the wildcard has a trailing '#'.
     """
 
     value: str = attrs.field()
     levels: List[str] = attrs.field(
-        repr=False, hash=False, default=attrs.Factory(_split, takes_self=True)
+        repr=False, hash=False, default=attrs.Factory(_split_wildcard, takes_self=True)
     )
+    prefix: bool = attrs.field(default=attrs.Factory(_has_hash, takes_self=True))
 
     def __str__(self) -> str:
         return self.value
@@ -57,68 +141,8 @@ class Wildcard:
                 "+" in level or "#" in level for level in self.levels if len(level) > 1
             )
         ):
-            msg = f"Invalid {self.__class__.__name__.lower()}: {self.value}"
+            msg = f"Invalid wildcard: {self.value}"
             raise ValueError(msg)
 
 
 WildcardLike: TypeAlias = "str | Wildcard"
-
-
-@attrs.frozen
-class Topic(Wildcard):
-    """MQTT topic that can be published and subscribed to.
-
-    Args:
-        value: The topic string.
-
-    Attributes:
-        value: The topic string.
-    """
-
-    value: str = attrs.field()
-    levels: List[str] = attrs.field(
-        repr=False, hash=False, default=attrs.Factory(_split, takes_self=True)
-    )
-
-    @value.validator
-    def check(self, attribute, value) -> None:
-        """Validate the topic."""
-        super()._check_value(attribute, value)
-        if "+" in value or "#" in value:
-            msg = f"Invalid topic: {value}"
-            raise ValueError(msg)
-
-    def matches(self, wildcard: WildcardLike) -> bool:
-        """Check if the topic matches a given wildcard.
-
-        Args:
-            wildcard: The wildcard to match against.
-
-        Returns:
-            True if the topic matches the wildcard, False otherwise.
-        """
-        if not isinstance(wildcard, Wildcard):
-            wildcard = Wildcard(wildcard)
-
-        topic_levels = self.levels
-        wildcard_levels = wildcard.levels
-        if wildcard_levels[0] == "$share":
-            # Shared subscriptions use the topic structure: $share/<group_id>/<topic>
-            wildcard_levels = wildcard_levels[2:]
-
-        # Special: 'a/b/c' matches the wildcard 'a/b/c/#'
-        if len(topic_levels) < len(wildcard_levels) - (wildcard_levels[-1] == "#"):
-            return False
-
-        # Otherwise, if the topic is longer than the wildcard it can't match
-        if len(wildcard_levels) < len(topic_levels) and wildcard_levels[-1] != "#":
-            return False
-
-        # now compare the individual pieces
-        for t, w in zip(topic_levels, wildcard_levels):
-            if t != w and w != "#" and w != "+":
-                return False
-        return True
-
-
-TopicLike: TypeAlias = "str | Topic"
